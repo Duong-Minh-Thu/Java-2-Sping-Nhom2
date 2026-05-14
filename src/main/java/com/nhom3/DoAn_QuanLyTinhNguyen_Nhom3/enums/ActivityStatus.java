1aba9392f53c7926fc099df33a0b544989dcb922
KugaNagisa <kuganagia@gmail.com>
2026-05-14 07:45:22 +0700

    Remove old root files, keep only frontend_ and volunteer-management

--- src/main/java/com/nhom3/DoAn_QuanLyTinhNguyen_Nhom3/enums/ActivityStatus.java
+++
@@ -1,5 +0,0 @@
-package com.nhom3.DoAn_QuanLyTinhNguyen_Nhom3.enums;
-
-public enum ActivityStatus {
-    UPCOMING, ONGOING, COMPLETED, CANCELLED
-}
--- src/main/java/com/nhom3/DoAn_QuanLyTinhNguyen_Nhom3/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.nhom3.DoAn_QuanLyTinhNguyen_Nhom3.enums;
-
-public enum Role {
-    ADMIN, ORG, STUDENT
-}
